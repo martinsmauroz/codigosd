8cbec8e7396c6fba4b6152221fec4b0684eb2996
Mauro Martins <martnismanutencao202@gmail.com>
2025-08-25 12:28:49 -0300

    atividade 17

---
+++ repetindo-dnv/atttt 17/atividade_17.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_button_pressed() -> void:
+	if $OptionButton.get_selected_id() == 0:
+		$Label.text = str(int($TextEdit.text) + int($TextEdit2.text))
+		
+	if $OptionButton.get_selected_id() == 1:
+		$Label.text = str(int($TextEdit.text) - int($TextEdit2.text))
+	
+	if $OptionButton.get_selected_id() == 2:
+		$Label.text = str(int($TextEdit.text) * int($TextEdit2.text))
+		
+	if $OptionButton.get_selected_id() == 3:
+		$Label.text = str(int($TextEdit.text) / int($TextEdit2.text))
